6ecd45f989d2b845ff96191962467cdd644d53d8
nghia <lyminhnghia@example.com>
2020-01-14 14:48:23 +0700

    update model

--- server/model/branch.model.js
+++ server/model/branch.model.js
@@ -1,8 +1,8 @@
 module.exports = (sequelize, Sequelize) => {
     const Branch = sequelize.define('branchs', {
-        name: Sequelize.TEXT + ' CHARSET utf8 COLLATE utf8_general_ci',
-        founding_date: Sequelize.DATE,
-        detail: Sequelize.TEXT + ' CHARSET utf8 COLLATE utf8_general_ci'
+        Tenchihoi: Sequelize.TEXT + ' CHARSET utf8 COLLATE utf8_general_ci',
+        Ngaythanhlap: Sequelize.DATE,
+        Chitiet: Sequelize.TEXT + ' CHARSET utf8 COLLATE utf8_general_ci'
     });
     return Branch;
 }
