5eee57306f0d96ceb091d6f0836b3a1f9a9efbb6
nghia <lyminhnghia@example.com>
2020-01-20 14:38:28 +0700

    update model

--- server/model/branch.model.js
+++ server/model/branch.model.js
@@ -1,9 +1,10 @@
 module.exports = (sequelize, Sequelize) => {
     const Branch = sequelize.define('branchs', {
-        Machihoi: Sequelize.STRING,
-        Tenchihoi: Sequelize.TEXT + ' CHARSET utf8 COLLATE utf8_general_ci',
-        Ngaythanhlap: Sequelize.DATE,
-        Chihoitruong: Sequelize.TEXT + ' CHARSET utf8 COLLATE utf8_general_ci'
+        Machihoi:       Sequelize.STRING,
+        Tenchihoi:      Sequelize.TEXT + ' CHARSET utf8 COLLATE utf8_general_ci',
+        Ngaythanhlap:   Sequelize.DATE,
+        Gioithieu:      Sequelize.TEXT + ' CHARSET utf8 COLLATE utf8_general_ci', 
+        Lichsu:         Sequelize.TEXT + ' CHARSET utf8 COLLATE utf8_general_ci'
     });
     return Branch;
 }
--- server/model/position.model.js
+++ server/model/position.model.js
@@ -1,7 +1,6 @@
 module.exports = (sequelize, Sequelize) => {
     const Position = sequelize.define('positions', {
-        Bacchuyenmon:       Sequelize.TEXT + ' CHARSET utf8 COLLATE utf8_general_ci',
-        Sogiotmau:          Sequelize.INTEGER
+        Chucvu:       Sequelize.TEXT + ' CHARSET utf8 COLLATE utf8_general_ci'
     });
     return Position;
 }
