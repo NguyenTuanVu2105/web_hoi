448e85c86dc65421f78438c09a7c11d6b4f2216e
Son Ngo <ngoson3899@gmail.com>
2020-02-11 20:40:16 +0700

    sửa AUDUnit

--- client/src/page/AUDUnit.js
+++ client/src/page/AUDUnit.js
@@ -1,11 +1,12 @@
 import React, { Component, useState, useContext, useEffect } from 'react'
 import HomepageContext from "../context/HomepageContext";
+import { AddUnitChild, ItemUnit } from '../Component/AddUnitChild'
 import { Select } from 'antd';
 import { Input } from 'antd';
 import '../css/AUDUnit.css'
 
 const AUDUnit = () => {
-    const [inputDisabled, setInputDisabled] = useState(false)
+    const [changeInput, setchangeInput] = useState(true)
     const [inputDisabledH, setInputDisabledH] = useState(true)
     const { nameMap, setNameMap } = useContext(HomepageContext)
 
@@ -25,23 +26,102 @@ const AUDUnit = () => {
         <div>
             <div className='changeAddUnit'>
                 <form>
-                    <Select defaultValue="disabled" style={{ width: 100, marginRight: 5, marginLeft: 20 }}>
-                        <Option style={{ textAlign: "center" }} value="disabled" disabled>Tùy chọn</Option>
-                        <Option style={{ textAlign: "center" }} value="1" onchange={()=>setInputDisabledH(false)}>Hội</Option>
-                        <Option style={{ textAlign: "center" }} value="2" onclick={()=>setInputDisabled(false)}>Chi hội</Option>
-                        <Option style={{ textAlign: "center" }} value="3" onclick={()=>setInputDisabled(false)}>Đội</Option>
-                    </Select>
+
                     <Search
                         placeholder="Tìm kiếm"
                         onSearch={value => console.log(value)}
                         style={{ width: 200, height: 30, marginRight: 5 }}
-                    />                   
-                    <button className="buttonDisable" disabled={inputDisabledH}>Sửa</button>
-                    <button className="buttonDisable" disabled={inputDisabledH}>Lưu thay đổi</button>
-                    <button className="buttonDisable" disabled={inputDisabled}>Thêm mới</button>
-                    <button className="buttonDisable" disabled={inputDisabled}>Xóa</button>
+                    />
+
 
                 </form>
+                <button className="buttonDisable" onClick={() => setchangeInput(false)}>Sửa</button>
+                <button className="buttonDisable" onClick={() => setchangeInput(true)}>Lưu thay đổi</button>
+                <button className="buttonDisable" onClick={() => setchangeInput(false)}>Thêm mới</button>
+                <button className="buttonDisable" onClick={() => setchangeInput(false)}>Xóa</button>
+            </div>
+            <div className="para">
+
+                {
+                    AddUnitChild.map(label => (
+                        <div>
+                            <span className="spanLabel">{label.name}</span>
+                            <input id="inputDisbleA" className="inputDisable" placeholder='1234' value='' disabled={changeInput} />
+                        </div>
+                    ))
+                }
+                <div>
+                    <span className="spanLabel">Thành viên hiện tại: </span>
+                </div>
+                <div className='row rowTable'>
+                    <table className='col-4 tableAddUnit' border={'1px'} cellpadding={'2px'}>
+                        <tr>
+                            <th>
+                                Cảm tình viên
+                        </th>
+                            <th className="inputTH">
+                                <input id="inputDisbleA" type='number' className="inputDisable" style={{ width: 50 }} placeholder='1234' disabled={changeInput} />
+                            </th>
+                        </tr>
+                        <tr>
+                            <th>
+                                Tình nguyện viên
+                        </th>
+                            <th className="inputTH">
+                                <input id="inputDisbleA" type='number' className="inputDisable" style={{ width: 50 }} placeholder='1234' disabled={changeInput} />
+                            </th>
+                        </tr>
+                        <tr>
+                            <th>
+                                Hội viên
+                        </th>
+                            <th className="inputTH">
+                                <input id="inputDisbleA"  type='number' className="inputDisable" style={{ width: 50 }} placeholder='1234' disabled={changeInput} />
+                            </th>
+                        </tr>
+                    </table>
+
+                    <table className='col-4 tableAddUnit' border={'1px'} cellpadding={'2px'}>
+                        <tr>
+                            <th>
+                                Hướng dẫn viên/Cán bộ tăng cường
+                        </th>
+                            <th className="inputTH">
+                                <input id="inputDisbleA" type='number' className="inputDisable" style={{ width: 50 }} placeholder='1234' disabled={changeInput} />
+                            </th>
+                        </tr>
+                        <tr>
+                            <th>
+                                Huấn luyện viên
+                        </th>
+                            <th className="inputTH">
+                                <input id="inputDisbleA" type='number' className="inputDisable" style={{ width: 50 }} placeholder='1234' disabled={changeInput} />
+                            </th>
+                        </tr>
+                        <tr>
+                            <th>
+                                Cán bộ
+                        </th>
+                            <th className="inputTH">
+                                <input id="inputDisbleA" type='number' className="inputDisable" style={{ width: 50 }} placeholder='1234' disabled={changeInput} />
+                            </th>
+                        </tr>
+                    </table>
+                </div>
+                <div>
+                    <span className="spanLabel">Số cơ sở trực thuộc Hội:</span><br />
+                    {
+                        ItemUnit.map(name => (
+                            <div style={{ paddingLeft: '20px' }}>
+                                <a>{name.name}</a><br />
+                            </div>
+                        ))
+                    }
+                </div>
+                <span className="spanLabel">Tổng số thành viên:</span><br />
+                <span className="spanLabel">Điểm hiến máu thường xuyên tổ chức:</span><br />
+                <span className="spanLabel">Kết quả hoạt động:</span>
+                <div></div>
             </div>
         </div>
     )
