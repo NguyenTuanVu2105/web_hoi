d2821428a468e3b84de329d7a5e4cf157d45e2d8
nghia <lyminhnghia@example.com>
2020-02-11 18:32:45 +0700

    update api

--- client/src/api/base/tablesearch.js
+++ client/src/api/base/tablesearch.js
@@ -2,7 +2,7 @@ import {createApiRequest} from '../index'
 
 export const getTableMember = (limit, page) => {
     return createApiRequest({
-        url: `http://localhost:5000/admin/view/member?limit=${limit}?&page=${page}`,
+        url: `/admin/view/member?limit=${limit}?&page=${page}`,
         method: 'get'
     })
 }
