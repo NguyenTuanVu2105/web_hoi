2a10ce188d29f2b438b31fbd67cf560de412c068
lyminhnghia <lmn2461999@gmail.com>
2020-02-25 11:32:06 +0700

    update api image

--- client/src/api/base/profile.js
+++ client/src/api/base/profile.js
@@ -2,15 +2,30 @@ import { createAuthApiRequest } from "../index"
 
 export const getUserProfile = () => {
   return createAuthApiRequest({
-      url: `/user/information/member`,
-      method: 'get'
+    url: `/user/information/member`,
+    method: 'get'
   })
 }
 
 export const updateUserProfile = (data) => {
   return createAuthApiRequest({
-      url: `/user/information/edit`,
-      method: 'put',
-      data: data
+    url: `/user/information/edit`,
+    method: 'put',
+    data: data
+  })
+}
+
+export const getAvatar = (fileName) => {
+  return createAuthApiRequest({
+    url: `/api/avatar/${fileName}`,
+    method: 'get'
+  })
+}
+
+export const uploadAvatar = (data) => {
+  return createAuthApiRequest({
+    url: `/api/upload/avatar`,
+    method: 'post',
+    data: data
   })
 }
