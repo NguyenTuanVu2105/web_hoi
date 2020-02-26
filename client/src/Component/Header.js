e76d5e3da27c658a50d8c9e3780f1d94e4e6c9e8
Son Ngo <ngoson3899@gmail.com>
2020-02-26 13:05:02 +0700

    update Hearder

--- client/src/Component/Header.js
+++ client/src/Component/Header.js
@@ -4,7 +4,19 @@ import '../css/Header.css'
 const Header = () =>{
     return(
         <div className='pageHeader'>
-            
+            <i className='fas fa-arrow-circle-left' style={{fontSize:45, color:'gray',opacity:'.8'}}/>
+            <div className="informationImg">
+                <label>Tên chương trình: Người Việt Trẻ 3000<a></a></label><br/>
+                <label>Ngày diễn ra: 30/2/3000 - 31/2/3000</label><br/>
+                <label>Địa điểm tổ chức: Hà Nội-Việt Nam</label>
+            </div>
+            <div className="triangleImg">
+
+            </div>
+            <div className="backgroundCover">
+
+            </div>
+            <i className='fas fa-arrow-circle-right' style={{fontSize:45, color:'gray',opacity:'.8'}}/>
         </div>
     )
 }
