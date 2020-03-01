ed1308bf35d7b2dc6b1e0c1c1c1d5d6a1b6008f7
NguyenTuanVu2105 <daigiachantran01@gmail.com>
2020-03-01 23:47:17 +0700

    deploy

--- server/index.js
+++ server/index.js
@@ -1,8 +1,16 @@
 var express = require('express')
 var app = express();
+var path = require('path')
 app.use(express.json());
 const cors = require('cors');
 app.use(cors())
+
+app.use(express.static(path.join(__dirname, '/../client/build')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname+'/../client/build/index.html'));
+});
+
 require('dotenv').config()
 
 require('./router/router')(app);
