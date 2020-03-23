f9bf2762c2a215b1dfa74d9e628430c7714f8d3a
Son Ngo <ngoson3899@gmail.com>
2020-03-23 11:06:48 +0700

    sửa lại css CBH

--- client/src/Component/CBH.js
+++ client/src/Component/CBH.js
@@ -10,42 +10,46 @@ const CBH = (props) => {
     }
 
     const changeBackgroudHeader = (event) => {
-        var target= event.target;
+        var target = event.target;
         var name = target.name;
         var value = target.value;
-        setInf({...inf,
+        setInf({
+            ...inf,
             [name]: value
         })
     }
     const [inf, setInf] = useState({
         name: 'Người Việt Trẻ 3000',
         link: 'https://www.facebook.com/',
-        linkAnh:'https://scontent.fhan4-1.fna.fbcdn.net/v/t1.0-9/85240841_2892567020796099_125429988689182720_o.jpg?_nc_cat=105&_nc_sid=dd9801&_nc_ohc=ZJ7DekKJR2sAX-dA-_M&_nc_ht=scontent.fhan4-1.fna&oh=47c35d57c93d60761ef95b8720d1c55a&oe=5EF562A8',
+        linkAnh: 'https://scontent.fhan4-1.fna.fbcdn.net/v/t1.0-9/85240841_2892567020796099_125429988689182720_o.jpg?_nc_cat=105&_nc_sid=dd9801&_nc_ohc=ZJ7DekKJR2sAX-dA-_M&_nc_ht=scontent.fhan4-1.fna&oh=47c35d57c93d60761ef95b8720d1c55a&oe=5EF562A8',
         time: '30/2/3000-31/2/3000',
         place: 'Hà Nội-Việt Nam',
         background: 'rgb(87, 78, 78)',
-        color: ''
+        color: 'white'
 
     })
-
+    const handleConfirm = () =>{
+        
+    }
     return (
-        <div className='pageHeader'id={props.id} style={{marginBottom:30}}>            
-            <div  className="informationImg" style={{backgroundColor:`${inf.background}`}}>                
-                <div>                                   
-                    <label name="name" className="labelHeader" style={{color:`${inf.color}`}}>Tên chương trình: <a href={inf.link} style={{color:`${inf.color}`}} target="blank">{inf.name}</a></label><br />
-                    <label name="time" className="labelHeader" style={{color:`${inf.color}`}}>Ngày diễn ra: {inf.time}</label><br />
-                    <label name="place" className="labelHeader" style={{color:`${inf.color}`}}>Địa điểm tổ chức: {inf.place}</label><br />                    
-                    <a className="doiBackground" data-toggle="modal" data-target={'#modalBackground'+props.id}> Changebackground >>></a>
+        <div className='pageHeader' id={props.id} style={{ marginBottom: 30 }}>
+            <div className="informationImg" style={{ backgroundColor: `${inf.background}` }}>
+                <div>
+                    <label name="time" className="labelHeader" style={{ color: `${inf.color}` }}>Tên chương trình:</label><br />
+                    <label name="name" className="labelHeader" style={{ color: `${inf.color}`, fontSize: 26 }}><a href={inf.link} style={{ color: `${inf.color}` }} target="blank">{inf.name}</a></label><br />
+                    <label name="time" className="labelHeader" style={{ color: `${inf.color}` }}>Ngày diễn ra: {inf.time}</label><br />
+                    <label name="place" className="labelHeader" style={{ color: `${inf.color}` }}>Địa điểm tổ chức: {inf.place}</label><br />
+                    <a className="doiBackground" data-toggle="modal" data-target={'#modalBackground' + props.id}> Changebackground >>></a>
                 </div>
 
-                <div className="modal fade AA" id={"modalBackground"+props.id} role="dialog">
+                <div className="modal fade AA" id={"modalBackground" + props.id} role="dialog">
                     <div className="modal-dialog">
                         <div className="modal-content">
                             <div className="modal-header">
                                 <h4>Chỉnh sửa background</h4>
                                 <button type="button" className="close" data-dismiss="modal">&times;</button>
                             </div>
-                            <div className="modal-body" style={{paddingBottom:0}}>
+                            <div className="modal-body" style={{ paddingBottom: 0 }}>
                                 {/* <form>
                                     <input type="text" name="name" className="changeBackground" placeholder="Tên chương trình" onChange={e=>changeBackgroudHeader(e)} />
                                     <input type="text" name="link" className="changeBackground" placeholder="Link chương trình" onChange={e=>changeBackgroudHeader(e)} />
@@ -57,18 +61,17 @@ const CBH = (props) => {
                                     <label className="changeColor">Màu chữ: </label>
                                     <input name="color" type="color" className="Ccolor"defaultValue="#ff0000" onChange={e=>changeBackgroudHeader(e)} /><br />
                                 </form> */}
-                                <Form>
+                                <Form >
                                     <Form.Item>
-                                        <Input type="text" name="name" style={{marginBottom:10}} placeholder="Tên chương trình" onChange={e=>changeBackgroudHeader(e)} />
-                                        <Input type="text" name="link" style={{marginBottom:10}} placeholder="Link chương trình" onChange={e=>changeBackgroudHeader(e)} />
-                                                                            
-                                        <Input type="text" name="time" style={{marginBottom:10}} placeholder="Ngày diễn ra" onChange={e=>changeBackgroudHeader(e)}/>
-                                        <Input type="date" name="hihi" style={{marginBottom:10}} placeholder="Ngày kết thúc" /> {/*sử lý gì tự sử lý đi nha */}
-                                        <Input type="text" name="place"  style={{marginBottom:10}} placeholder="Địa điểm tổ chức" onChange={e=>changeBackgroudHeader(e)} />
+                                        <Input type="text" name="name" style={{ marginBottom: 10 }} placeholder="Tên chương trình" onChange={e => changeBackgroudHeader(e)} required/>
+                                        <Input type="text" name="link" style={{ marginBottom: 10 }} placeholder="Link chương trình" onChange={e => changeBackgroudHeader(e)}/>
+                                        <Input type="text" name="time" style={{ marginBottom: 10 }} placeholder="Ngày diễn ra - Ngày kết thúc" onChange={e => changeBackgroudHeader(e)}/>
+                                        <Input type="date" name="hihi" style={{ marginBottom: 10 }} placeholder="Ngày kết thúc" required/> {/*sử lý gì tự sử lý đi nha */}
+                                        <Input type="text" name="place" style={{ marginBottom: 10 }} placeholder="Địa điểm tổ chức" onChange={e => changeBackgroudHeader(e)}/>
                                         <label className="changeColor">Màu nền: </label>
-                                        <Input name="background" type="color" style={{marginBottom:10, width:80,marginLeft:5}} defaultValue="#ff0000" onChange={e=>changeBackgroudHeader(e)}/><br />
+                                        <Input name="background" type="color" style={{ marginBottom: 10, width: 80, marginLeft: 5 }} defaultValue="#ff0000" onChange={e => changeBackgroudHeader(e)} /><br />
                                         <label className="changeColor">Màu chữ: </label>
-                                        <Input name="color" type="color" style={{marginBottom:10, width:80,marginLeft:5}} defaultValue="#ff0000" onChange={e=>changeBackgroudHeader(e)} /><br />
+                                        <Input name="color" type="color" style={{ marginBottom: 10, width: 80, marginLeft: 5 }} defaultValue="#ff0000" onChange={e => changeBackgroudHeader(e)} /><br />
                                         <Upload
                                             // link to upload
                                             customRequest={onChooseFile}
@@ -77,11 +80,11 @@ const CBH = (props) => {
                                             multiple={false}
                                             fileList={[]}
                                         >
-                                            <div style={{display:'flex',flexWrap:'wrap'}}><label className="changeColor">Tải ảnh lên: </label><Icon style={{marginLeft:5}} type="camera" theme="filled" className="icon_change_avatar" /></div>                              
+                                            <div style={{ display: 'flex', flexWrap: 'wrap', marginBottom: 10 }}><label className="changeColor">Tải ảnh lên: </label><Icon style={{ marginLeft: 5 }} type="camera" theme="filled" className="icon_change_avatar" /></div>
                                             {/* <p style={{ textAlign: "center", color: "white", width: "100%" }}>Thay đổi</p> */}
-                                        </Upload>  
-                                        <div className="modal-footer" style={{paddingBottom:0}}>
-                                            <Button type="button" className="footerButton" data-dismiss="modal">Lưu thay đổi</Button>
+                                        </Upload>
+                                        <div className="modal-footer" style={{ paddingBottom: 0 }}>
+                                            <Button type="submit" className="footerButton"  onClick={()=>handleConfirm()}>Lưu thay đổi</Button>
                                         </div>
                                     </Form.Item>
                                 </Form>
@@ -94,9 +97,9 @@ const CBH = (props) => {
                 </div>
             </div>
             <div>
-                <div className="triangleImg" style={{borderLeft:`60px solid ${inf.background}`}}></div>
-                <div name="linkAnh" className="backgroundCover" style={{backgroundImage:`url(${inf.linkAnh})`}}></div>
-            </div>            
+                <div className="triangleImg" style={{ borderLeft: `60px solid ${inf.background}` }}></div>
+                <div name="linkAnh" className="backgroundCover" style={{ backgroundImage: `url(${inf.linkAnh})` }}></div>
+            </div>
         </div>
     )
 }
--- client/src/Component/Header.js
+++ client/src/Component/Header.js
@@ -5,32 +5,28 @@ import '../css/Header.css'
 const Header = (props) => {
     const [inf, setInf] = useState({
         name: 'Người Việt Trẻ 3000',
-        link: 'https://www.facebook.com/',
+        link: 'https://www.facebook.com/nguoiviettrevietnam/',
         time: '30/2/3000-31/2/3000',
         place: 'Hà Nội-Việt Nam',
         background: 'rgb(87, 78, 78)',
-        color: ''
+        color: 'white'
 
     })
 
     return (
         <div className='pageHeader'>            
             <div  className="informationImg" style={{backgroundColor:`${inf.background}`}}>                
-                <div>                     
-                    <label name="name" className="labelHeader" style={{color:`${inf.color}`}}>Tên chương trình: <a href={inf.link} style={{color:`${inf.color}`}} target="blank">{inf.name}</a></label><br />
+                <div>      
+                    <label name="time" className="labelHeader" style={{color:`${inf.color}`}}>Tên chương trình:</label><br />               
+                    <label name="name" className="labelHeader" style={{color:`${inf.color}`, fontSize:26}}> <a href={inf.link} style={{color:`${inf.color}`}} target="blank">{inf.name}</a></label><br />
                     <label name="time" className="labelHeader" style={{color:`${inf.color}`}}>Ngày diễn ra: {inf.time}</label><br />
                     <label name="place" className="labelHeader" style={{color:`${inf.color}`}}>Địa điểm tổ chức: {inf.place}</label><br />                    
                 </div>
             </div>
             <div>
-                <div className="triangleImg" style={{borderLeft:`60px solid ${inf.background}`}}>
-
-                </div>
-                <div className="backgroundCover">
-
-                </div>
+                <div className="triangleImg" style={{borderLeft:`60px solid ${inf.background}`}}/>
+                <div className="backgroundCover"/>
             </div>            
-            {/* <i className='fas fa-arrow-circle-right' style={{ fontSize: 45, color: 'gray', opacity: '.8' }} /> */}
         </div>
     )
 }
