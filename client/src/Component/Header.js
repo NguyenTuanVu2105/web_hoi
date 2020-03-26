c2de25cf3c83b7a3f94915d3ae0b260013c40099
Son Ngo <ngoson3899@gmail.com>
2020-03-26 21:34:23 +0700

    sửa lại phần header

--- client/src/Component/Header.js
+++ client/src/Component/Header.js
@@ -10,23 +10,22 @@ const Header = (props) => {
         place: 'Hà Nội-Việt Nam',
         background: 'rgb(87, 78, 78)',
         color: 'white'
-
     })
-
+    console.log(props)
     return (
-        <div className='pageHeader'>            
-            <div  className="informationImg" style={{backgroundColor:`${inf.background}`}}>                
+        <div className='backgroundCover' style={props} >            
+            {/* <div  className="informationImg" style={{backgroundColor:`${inf.background}`}}>                
                 <div>      
                     <label name="time" className="labelHeader" style={{color:`${inf.color}`}}>Tên chương trình:</label><br />               
                     <label name="name" className="labelHeader" style={{color:`${inf.color}`, fontSize:26}}> <a href={inf.link} style={{color:`${inf.color}`}} target="blank">{inf.name}</a></label><br />
                     <label name="time" className="labelHeader" style={{color:`${inf.color}`}}>Ngày diễn ra: {inf.time}</label><br />
                     <label name="place" className="labelHeader" style={{color:`${inf.color}`}}>Địa điểm tổ chức: {inf.place}</label><br />                    
                 </div>
-            </div>
-            <div>
+            </div> */}
+            {/* <div>
                 <div className="triangleImg" style={{borderLeft:`60px solid ${inf.background}`}}/>
                 <div className="backgroundCover"/>
-            </div>            
+            </div>             */}
         </div>
     )
 }
--- client/src/Component/slideshowHeader.js
+++ client/src/Component/slideshowHeader.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Slide } from 'react-slideshow-image';
 import Header from '../Component/Header';
-
+import '../css/Header.css'
 
 const properties = {
   duration: 3500,
@@ -14,30 +14,40 @@ const properties = {
   }
 }
 const arr = []
-for (var i = 0; i < 4; i++){
+for (var i = 0; i < 4; i++) {
   arr.push(<div key={i.toString()} className="each-slide">
-  <Header/>
-</div>)
-} 
+    <Header />
+  </div>)
+}
+const arr1 = []
+for (var i = 0; i < 4; i++) {
+  arr1.push(<div key={i.toString()} className="each-slide">
+    <div className="chil-cont">
+      <Header width="340px" height="135px" />
+    </div>
+    
+  </div>)
+}
+
 const Slideshow = () => {
-    return (
-      <div className="slide-container" style={{width:'93%',maxWidth:1165,margin:'0 auto'}}>
+  return (
+    <div style={{display:'flex', flexWrap:'wrap',marginLeft:15, marginRight:15}}>
+      <div className="slide-container" style={{ width: 900}}>
         <Slide {...properties}>
-          {/* <div className="each-slide">
-            <Header/>
-          </div>
-          <div className="each-slide">
-            <Header/>
-          </div>
-          <div className="each-slide">
-            <Header/>
-          </div> */}
           {
             arr
           }
         </Slide>
-        
       </div>
-    )
+      <div className="menu-cont" id="scroll-menu">
+
+          {
+            arr1
+          }
+
+      </div>
+    </div>
+
+  )
 }
 export default Slideshow;
