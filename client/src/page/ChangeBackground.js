b948c0988597db364c2c78294e82aad819f90170
lyminhnghia <lmn2461999@gmail.com>
2020-03-29 16:05:05 +0700

    fix

--- client/src/page/ChangeBackground.js
+++ client/src/page/ChangeBackground.js
@@ -25,48 +25,20 @@ const ChangeBackground = () => {
             ['/HistoryBlood']: 'Change Background',
         })
     }, [])
-    const [count, setCount] = useState(10)
+    const [count, setCount] = useState(0)
 
     const arr = []
-    const arr1 = []
-    const arr2 = []
-    var temp = 0
-    for (var i = 1; i <= count; i++) {
-        arr1.push(<CBH id={i} />)
-        temp +=1
-        if(temp % 5==0){
-            arr.push(
-                <div id={"id-"+ i}>
-                    {
-                        arr1
-                    }
-                </div>
-            )
-            arr2.push(
-                <a className="pagination-a" id={"a-"+i} href={"#id-"+i}>{i/5}</a>
-            )      
-        }
+    for (let i = 0; i < count; i++) {
+        arr.push(<CBH id={i} />)
     }
-
     return (
         <div className="para">
             <button className="addBackground" onClick={() => setCount(1)}>Thêm background</button>
             {
                 arr
             }
-            <div class="container">
-                <div class="pagination">
-                    <a className="pagination-a" href="#">&laquo;</a>
-                    {/* <a className="pagination-a active" href="#">1</a> */}
-                    {
-                        arr2
-                    }
-                    <a className="pagination-a" href="#">&raquo;</a>
-                </div>
-            </div>
-
         </div>
     )
 }
 
-export default ChangeBackground;
+export default ChangeBackground
