ab466f120ea4b3629e70a908890c2dcb43a22992
Son Ngo <ngoson3899@gmail.com>
2020-03-29 13:44:58 +0700

    thêm pagination trong changebackground

--- client/src/page/ChangeBackground.js
+++ client/src/page/ChangeBackground.js
@@ -1,7 +1,7 @@
-import React, { Component,useState, useContext, useEffect } from 'react'
+import React, { Component, useState, useContext, useEffect } from 'react'
 import HomepageContext from "../context/HomepageContext";
 import CBH from "../Component/CBH";
-import '../css/changeBackground.css'
+import '../css/changeBackground.scss'
 import { getAllBackground } from '../api/base/background'
 const ChangeBackground = () => {
     const { nameMap, setNameMap, setLoading } = useContext(HomepageContext)
@@ -25,18 +25,46 @@ const ChangeBackground = () => {
             ['/HistoryBlood']: 'Change Background',
         })
     }, [])
-    const [count, setCount] = useState(0)
+    const [count, setCount] = useState(10)
 
     const arr = []
-    for (var i = 0; i<count;i++){
-        arr.push(<CBH id={i}/>)
+    const arr1 = []
+    const arr2 = []
+    var temp = 0
+    for (var i = 1; i <= count; i++) {
+        arr1.push(<CBH id={i} />)
+        temp +=1
+        if(temp % 5==0){
+            arr.push(
+                <div id={"id-"+ i}>
+                    {
+                        arr1
+                    }
+                </div>
+            )
+            arr2.push(
+                <a className="pagination-a" id={"a-"+i} href={"#id-"+i}>{i/5}</a>
+            )      
+        }
     }
+
     return (
         <div className="para">
-            <button className="addBackground" onClick={()=>setCount(1)}>Thêm background</button>
+            <button className="addBackground" onClick={() => setCount(1)}>Thêm background</button>
             {
                 arr
             }
+            <div class="container">
+                <div class="pagination">
+                    <a className="pagination-a" href="#">&laquo;</a>
+                    {/* <a className="pagination-a active" href="#">1</a> */}
+                    {
+                        arr2
+                    }
+                    <a className="pagination-a" href="#">&raquo;</a>
+                </div>
+            </div>
+
         </div>
     )
 }
