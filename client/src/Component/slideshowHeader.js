8777893a3e70f243db33ffa3c1a3b71b6a065dfe
Son Ngo <ngoson3899@gmail.com>
2020-05-10 14:34:38 +0700

    sửa slider

--- client/src/Component/slideshowHeader.js
+++ client/src/Component/slideshowHeader.js
@@ -37,17 +37,17 @@ console.log(inf)
             inf.map(inf => (
               <div className="each-slide">
                 <div className='pageHeader'>
-                  <div className="informationImg" style={{ backgroundColor: `${child.Maunen}` }}>
+                  <div className="informationImg" style={{ backgroundColor: `${inf.Maunen}` }}>
                     <div>
-                      <label name="time" className="labelHeader" style={{ color: `${child.Mauchu}` }}>Tên chương trình:</label><br />
-                      <label name="name" className="labelHeader" style={{ color: `${child.Mauchu}`, fontSize: 26 }}> <a href={child.Linkanh} style={{ color: `${child.Mauchu}` }} target="blank">{child.Tenchuongtrinh}</a></label><br />
-                      <label name="time" className="labelHeader" style={{ color: `${child.Mauchu}` }}>Ngày diễn ra: {child.Ngaydienra}</label><br />
-                      <label name="place" className="labelHeader" style={{ color: `${child.Mauchu}` }}>Địa điểm tổ chức: {child.Diadiem}</label><br />
+                      <label name="time" className="labelHeader" style={{ color: `${inf.Mauchu}` }}>Tên chương trình:</label><br />
+                      <label name="name" className="labelHeader" style={{ color: `${inf.Mauchu}`, fontSize: 26 }}> <a href={inf.Linkanh} style={{ color: `${inf.Mauchu}` }} target="blank">{inf.Tenchuongtrinh}</a></label><br />
+                      <label name="time" className="labelHeader" style={{ color: `${inf.Mauchu}` }}>Ngày diễn ra: {inf.Ngaydienra}</label><br />
+                      <label name="place" className="labelHeader" style={{ color: `${inf.Mauchu}` }}>Địa điểm tổ chức: {inf.Diadiem}</label><br />
                     </div>
                   </div>
                   <div>
-                    <div className="triangleImg" style={{ borderLeft: `60px solid ${child.Maunen}` }} />
-                    <div className="backgroundCover" style={{backgroundImage: `url(${child.Linkanh})`}} />
+                    <div className="triangleImg" style={{ borderLeft: `60px solid ${inf.Maunen}` }} />
+                    <div className="backgroundCover" style={{backgroundImage: `url(${inf.Linkanh})`}} />
                   </div>
                 </div>
               </div>
