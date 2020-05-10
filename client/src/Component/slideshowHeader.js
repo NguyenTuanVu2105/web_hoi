ac143684219e6cc9e4055f06ec1c349e2d64f479
Son Ngo <ngoson3899@gmail.com>
2020-05-10 14:26:11 +0700

    sửa slider

--- client/src/Component/slideshowHeader.js
+++ client/src/Component/slideshowHeader.js
@@ -14,7 +14,7 @@ const properties = {
   }
 }
 const Slideshow = () => {
-  const [inf, setInf] = useState(null)
+  const [inf, setInf] = useState([])
 
   const fetchData = async () => {
     const result = await getAllBackground()
@@ -25,26 +25,26 @@ const Slideshow = () => {
   useEffect(() => {
     fetchData()
   }, [])
-
+console.log(inf)
   return (
     <div style={{ display: 'flex', flexWrap: 'wrap', marginLeft: 15, marginRight: 15 }}>
       <div className="slide-container" style={{ width: 1200, margin: "0 auto" }}>
         <Slide {...properties}>
           {
-            inf.map((child, index) => (
+            inf.map(child => (
               <div className="each-slide">
                 <div className='pageHeader'>
-                  <div className="informationImg" style={{ backgroundColor: `${inf.Maunen}` }}>
+                  <div className="informationImg" style={{ backgroundColor: `${child.Maunen}` }}>
                     <div>
-                      <label name="time" className="labelHeader" style={{ color: `${inf.Mauchu}` }}>Tên chương trình:</label><br />
-                      <label name="name" className="labelHeader" style={{ color: `${inf.Mauchu}`, fontSize: 26 }}> <a href={inf.Linkanh} style={{ color: `${inf.Mauchu}` }} target="blank">{inf.Tenchuongtrinh}</a></label><br />
-                      <label name="time" className="labelHeader" style={{ color: `${inf.Mauchu}` }}>Ngày diễn ra: {inf.Ngaydienra}</label><br />
-                      <label name="place" className="labelHeader" style={{ color: `${inf.Mauchu}` }}>Địa điểm tổ chức: {inf.Diadiem}</label><br />
+                      <label name="time" className="labelHeader" style={{ color: `${child.Mauchu}` }}>Tên chương trình:</label><br />
+                      <label name="name" className="labelHeader" style={{ color: `${child.Mauchu}`, fontSize: 26 }}> <a href={child.Linkanh} style={{ color: `${child.Mauchu}` }} target="blank">{child.Tenchuongtrinh}</a></label><br />
+                      <label name="time" className="labelHeader" style={{ color: `${child.Mauchu}` }}>Ngày diễn ra: {child.Ngaydienra}</label><br />
+                      <label name="place" className="labelHeader" style={{ color: `${child.Mauchu}` }}>Địa điểm tổ chức: {child.Diadiem}</label><br />
                     </div>
                   </div>
                   <div>
-                    <div className="triangleImg" style={{ borderLeft: `60px solid ${inf.Maunen}` }} />
-                    <div className="backgroundCover" style={{backgroundImage: `url(${inf.Linkanh})`}} />
+                    <div className="triangleImg" style={{ borderLeft: `60px solid ${child.Maunen}` }} />
+                    <div className="backgroundCover" style={{backgroundImage: `url(${child.Linkanh})`}} />
                   </div>
                 </div>
               </div>
