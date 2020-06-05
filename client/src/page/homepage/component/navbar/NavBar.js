63ea4b1b63283dd771c32375fb314bc87f797c69
Son Ngo <ngoson3899@gmail.com>
2020-06-05 10:38:04 +0700

    update ui navbar

--- client/src/page/homepage/component/navbar/NavBar.js
+++ client/src/page/homepage/component/navbar/NavBar.js
@@ -82,7 +82,7 @@ const NavBar = () => {
                         </a>
                     </div>
                     <div id="collapse7" className="panel-collapse collapse">
-                        <ul className="list-group">
+                        <ul className="list-group" style={{paddingLeft:"5%", paddingRight:"5%"}}>
                             <li className="list-group-item"><a className="list-items" onClick={logout} style={{color:"#ff4d4d"}}>ĐĂNG XUẤT</a></li>
                             <li className="list-group-item"><a className="list-items" data-toggle="modal" data-target="#modalMK"  style={{color:"#ff4d4d"}}>ĐỔI MẬT KHẨU</a></li>
                         </ul>
