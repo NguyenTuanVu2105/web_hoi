00a9e95a616b8e991cbc1230398f8fea342cf848
Son Ngo <ngoson3899@gmail.com>
2020-06-09 14:53:49 +0700

    xoa code thua slide

--- client/src/page/homepage/component/header/slideshowHeader.js
+++ client/src/page/homepage/component/header/slideshowHeader.js
@@ -1,4 +1,4 @@
-import React, {useContext, useEffect, useState} from 'react'
+import React, {useContext } from 'react'
 import { Slide } from 'react-slideshow-image';
 import './Header.css'
 import HomepageContext from "../../../../context/HomepageContext";
