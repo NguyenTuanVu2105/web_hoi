7136bf8a6343174d1a8896a8b8dc651a5d87adf6
lyminhnghia <=>
2020-12-18 15:17:27 +0700

    fix input selector

--- client/src/components/Input/InputSelector.js
+++ client/src/components/Input/InputSelector.js
@@ -2,12 +2,13 @@ import React, { memo } from "react";
 import { TextField } from "@material-ui/core";
 import { Autocomplete } from "@material-ui/lab";
 
-const InputSelector = ({ data, label, ...props }) => {
+const InputSelector = ({ className, data, label, ...props }) => {
   return (
     <Autocomplete
       options={data}
-      getOptionLabel={(option) => option.title}
-      style={{ width: 300 }}
+      getOptionLabel={(option) => option.name}
+      className={className}
+      size="small"
       renderInput={(params) => (
         <TextField {...params} label={label} variant="outlined" />
       )}
