3d169b226a75bd928c7ce39d6348387a8bfc2020
Son Ngo <ngoson3899@gmail.com>
2020-12-29 15:09:38 +0700

    fix ui ho so to chuc

--- client/src/pages/Organizational/UnitRecord/index.js
+++ client/src/pages/Organizational/UnitRecord/index.js
@@ -2,7 +2,7 @@ import React, { useContext, useEffect, useState } from "react";
 import HomepageContext from "../../../context/HomepageContext";
 import { Unit, Admin } from "../../../api/base";
 import { Link } from "react-router-dom";
-import "./style.css";
+import "./style.scss";
 
 const UnitRecord = () => {
   const { setNameMap, setLoading } = useContext(HomepageContext);
@@ -35,75 +35,78 @@ const UnitRecord = () => {
     });
   }, []);
   return (
-    <div className="para-unit-profile">
-      {unit.map((search, index) => (
-        <div key={"U-" + index}>
-          <a
-            className="panel-heading list-group-blood "
-            data-toggle="collapse"
-            href={`#${search.Machihoi}`}
-          >
-            <div className="AFM">
-              {search.Tenchihoi}
-              <i
-                id="icon10"
-                className="fa fa-angle-down"
-                style={{ fontSize: "25px" }}
-              />
-            </div>
-          </a>
-          <div
-            id={search.Machihoi}
-            className="panel-collapse collapse"
-            style={{ marginTop: "10px" }}
-          >
-            <ul className="list-group">
-              <li key={"C-1"} className="list-group-blood-item">
-                <div style={{ width: "100%" }}>
-                  <Link
-                    className="list-items-a"
-                    to={{
-                      pathname: `/ho-so-chi-hoi/${search.Machihoi}`,
-                    }}
-                  >
-                    {" "}
-                    Giới thiệu về {search.Tenchihoi}
-                    <div style={{ display: "flex" }}>
-                      <i
-                        className="fa fa-caret-right"
-                        style={{ fontSize: "20px" }}
-                      />
-                    </div>
-                  </Link>
-                </div>
-              </li>
-            </ul>
-            <ul className="list-group">
-              {search.clubs.map((child, index) => (
-                <li key={"C-" + index} className="list-group-blood-item">
-                  {/* <div style={{width:"100%"}}> */}
-                  <Link
-                    className="list-items-a"
-                    to={{
-                      pathname: `/ho-so-doi-mau/${child.Madoi}`,
-                    }}
-                  >
-                    {" "}
-                    {child.Tendoi}
-                    <div style={{ display: "flex" }}>
-                      <i
-                        className="fa fa-caret-right"
-                        style={{ fontSize: "20px" }}
-                      />
-                    </div>
-                  </Link>
-                  {/* </div> */}
+    <div className="box-para-unit">
+      <div className="box-link-unit">
+        {unit.map((search, index) => (
+          <div key={"U-" + index}>
+            <a
+              className="panel-heading list-group-blood "
+              data-toggle="collapse"
+              href={`#${search.Machihoi}`}
+            >
+              <div className="box-name-unit">
+                {search.Tenchihoi}
+                <i
+                  id="icon10"
+                  className="fa fa-angle-down"
+                  style={{ fontSize: "25px" }}
+                />
+              </div>
+            </a>
+            <div
+              id={search.Machihoi}
+              className="panel-collapse collapse"
+              // style={{ marginTop: "10px" }}
+            >
+              <ul className="list-group">
+                <li key={"C-1"} className="list-group-blood-item">
+                  <div style={{ width: "100%" }}>
+                    <Link
+                      className="box-item-unit"
+                      to={{
+                        pathname: `/ho-so-chi-hoi/${search.Machihoi}`,
+                      }}
+                    >
+                      {" "}
+                      Giới thiệu về {search.Tenchihoi}
+                      <div style={{ display: "flex" }}>
+                        <i
+                          className="fa fa-caret-right"
+                          style={{ fontSize: "14px" }}
+                        />
+                      </div>
+                    </Link>
+                  </div>
                 </li>
-              ))}
-            </ul>
+              </ul>
+              <ul className="list-group">
+                {search.clubs.map((child, index) => (
+                  <li key={"C-" + index} className="list-group-blood-item">
+                    {/* <div style={{width:"100%"}}> */}
+                    <Link
+                      className="box-item-unit"
+                      to={{
+                        pathname: `/ho-so-doi-mau/${child.Madoi}`,
+                      }}
+                    >
+                      {" "}
+                      {child.Tendoi}
+                      <div style={{ display: "flex" }}>
+                        <i
+                          className="fa fa-caret-right"
+                          style={{ fontSize: "14px" }}
+                        />
+                      </div>
+                    </Link>
+                    {/* </div> */}
+                  </li>
+                ))}
+              </ul>
+            </div>
           </div>
-        </div>
-      ))}
+        ))}
+      </div>
+      <div></div>
     </div>
   );
 };
