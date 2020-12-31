76099f7c79b4efb2ed71b88fed8c8e0723460f2b
nghialm99 <nghialm@bglobalcorp.com>
2020-12-31 16:03:31 +0700

    update async await position controller

--- server/controller/position.controller.js
+++ server/controller/position.controller.js
@@ -1,100 +1,95 @@
-const db = require('../config/db.config')
-const Position = db.position
-const User = db.user
-const Op = db.Sequelize.Op
+const db = require("../config/db.config");
+const Position = db.position;
+const User = db.user;
+const Op = db.Sequelize.Op;
 
-exports.viewPosition = (req, res) => {
-    User.findOne({
+exports.viewPosition = async (req, res) => {
+  try {
+    let user = await User.findOne({
+      where: {
+        id: req.userId,
+      },
+    });
+    if (user.role === "hoitruong") {
+      let position = await Position.findAll({
+        attributes: ["id", "Chucvu"],
+      });
+      res.status(200).send({ success: true, data: position });
+    } else if (user.role === "chihoitruong") {
+      let position = await Position.findAll({
         where: {
-            id: req.userId
-        }
-    }).then(user => {
-        if (user.role === 'hoitruong') {
-            Position.findAll({
-                attributes: ['id', 'Chucvu']
-            }).then(position => {
-                res.status(200).send({success: true, data: position})
-            }).catch(err => {
-                res.status(500).send({success: false, message: err})
-            })
-        } else if (user.role === 'chihoitruong') {
-            Position.findAll({
-                where: {
-                    Capbac: {
-                        [Op.ne]: 1
-                    }
-                },
-                attributes: ['id', 'Chucvu']
-            }).then(position => {
-                res.status(200).send({success: true, data: position})
-            }).catch(err => {
-                res.status(500).send({success: false, message: err})
-            })
-        } else {
-            Position.findAll({
-                where: {
-                    Capbac: {
-                        [Op.eq]: 3
-                    }
-                },
-                attributes: ['id', 'Chucvu']
-            }).then(position => {
-                res.status(200).send({success: true, data: position})
-            }).catch(err => {
-                res.status(500).send({success: false, message: err})
-            })
-        }
-    }).catch(err => {
-        res.status(500).send({success: false, message: err})
-    })
-}
+          Capbac: {
+            [Op.ne]: 1,
+          },
+        },
+        attributes: ["id", "Chucvu"],
+      });
+      res.status(200).send({ success: true, data: position });
+    } else {
+      let position = await Position.findAll({
+        where: {
+          Capbac: {
+            [Op.eq]: 3,
+          },
+        },
+        attributes: ["id", "Chucvu"],
+      });
+      res.status(200).send({ success: true, data: position });
+    }
+  } catch (error) {
+    res.status(500).send({ success: false, message: "Error -> " + error });
+  }
+};
 
-exports.addPosition = (req, res) => {
-    Position.findOne({
-        where: {
-            Chucvu: req.body.chucvu,
-            Capbac: req.body.capbac
-        }
-    }).then(positions => {
-        if (!positions) {
-            Position.create({
-                Chucvu: req.body.chucvu,
-                Capbac: req.body.capbac
-            }).then(position => {
-                res.status(200).send({success: true})
-            }).catch(err => {
-                res.status(500).send({success: false, message: err})
-            })
-        } else {
-            res.status(404).send({success: false, message: "Position is exist"})
-        }
-    }).catch(err => {
-        res.status(500).send({success: false, message: err})
-    })
-}
+exports.addPosition = async (req, res) => {
+  try {
+    let positions = await Position.findOne({
+      where: {
+        Chucvu: req.body.chucvu,
+        Capbac: req.body.capbac,
+      },
+    });
+    if (!positions) {
+      let position = await Position.create({
+        Chucvu: req.body.chucvu,
+        Capbac: req.body.capbac,
+      });
+      res.status(200).send({ success: true, data: position });
+    } else {
+      res.status(404).send({ success: false, message: "Position is exist!" });
+    }
+  } catch (error) {
+    res.status(500).send({ success: false, message: "Error -> " + error });
+  }
+};
 
-exports.editPosition = (req, res) => {
-    Position.update({
-        Chucvu: req.body.chucvu
-    }, {
+exports.editPosition = async (req, res) => {
+  try {
+    let position = await Position.update(
+      {
+        Chucvu: req.body.chucvu,
+      },
+      {
         where: {
-            id: req.body.id
-        }
-    }).then(position => {
-        res.status(200).send({success: true})
-    }).catch(err => {
-        res.status(500).send({success: false, message: err})
-    })
-}
+          id: req.body.id,
+        },
+      }
+    );
+    res.status(200).send({ success: true, data: position });
+  } catch (error) {
+    res.status(500).send({ success: false, message: "Error -> " + error });
+  }
+};
 
-exports.deletePosition = (req, res) => {
-    position.delete({
-        where: {
-            id: res.body.id
-        }
-    }).then(position => {
-        res.status(200).send({success: true})
-    }).catch(err => {
-        res.status(500).send({success: false, message: err})
-    })
-}
+exports.deletePosition = async (req, res) => {
+  try {
+    await Position.delete({
+      where: {
+        id: res.body.id,
+      },
+    });
+    res.status(200).send({ success: true, data: "Deleted is success!" });
+  } catch (error) {
+    res.status(500).send({ success: false, message: "Error -> " + error });
+  }
+};
