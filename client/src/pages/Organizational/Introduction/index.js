7f0bd6bff9b7fc84bfb0152cf9d26483fba7d337
Son Ngo <ngoson3899@gmail.com>
2021-01-03 00:23:28 +0700

    fix path img introduction

--- client/src/pages/Organizational/Introduction/index.js
+++ client/src/pages/Organizational/Introduction/index.js
@@ -111,7 +111,7 @@ const Introduction = (props) => {
               <div className="logo-intro-reponsive">
                 <img
                   className="logo-intro-img-rp"
-                  src="./img/navbar/logomau.png"
+                  src="./img/logomau.png"
                   alt="logo"
                 />
                 <div className="intro-tag-a-rp">
@@ -420,7 +420,7 @@ const Introduction = (props) => {
               <div className="logo-intro">
                 <img
                   className="logo-intro-img"
-                  src="./img/navbar/logomau.png"
+                  src="./img/logomau.png"
                   alt="logo"
                 />
                 <div className="intro-tag-a">
