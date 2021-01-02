3c6e4dbdd4fcf779f6c51e3c48ce05b2b2f15e59
nghialm99 <nghialm@bglobalcorp.com>
2021-01-02 19:34:40 +0700

    edit format code in file association, background, branch controller

--- server/controller/association.controller.js
+++ server/controller/association.controller.js
@@ -1,32 +1,54 @@
 const db = require("../config/db.config");
 const Association = db.association;
-// const Activity      = db.activity
-// const Remunerative  = db.remunerative
 
-exports.ViewAssociation = (req, res) => {
-  Association.findOne({
-    where: {
-      id: 1,
-    },
-  })
-    .then((association) => {
-      res.status(200).send({ success: true, message: association });
-    })
-    .catch((err) => {
-      res.status(500).send({ success: false, message: err });
+exports.ViewAssociation = async (req, res) => {
+  try {
+    let association = await Association.findOne({
+      where: {
+        id: 1,
+      },
     });
+    res.status(200).send({ success: true, message: association });
+  } catch (error) {
+    res.status(500).send({ success: false, message: "Error -> " + error });
+  }
 };
 
-exports.UpdateAssociation = (req, res) => {
-  Association.findOne({
-    where: {
-      id: 1,
-    },
-  })
-    .then((association) => {
-      if (!association) {
-        Association.create({
-          id: 1,
+exports.UpdateAssociation = async (req, res) => {
+  try {
+    let association = await Association.findOne({
+      where: {
+        id: 1,
+      },
+    });
+    if (!association) {
+      Association.create({
+        id: 1,
+        Chutichhoi: req.body.Chutichhoi,
+        Phochutich_1: req.body.Phochutich_1,
+        Phochutich_2: req.body.Phochutich_2,
+        Phochutich_3: req.body.Phochutich_3,
+        Phochutich_4: req.body.Phochutich_4,
+        Uyvien_1: req.body.Uyvien_1,
+        Uyvien_2: req.body.Uyvien_2,
+        Namthanhlap: req.body.Namthanhlap,
+        Ngaytruyenthong: req.body.Ngaytruyenthong,
+        Camtinhvien: req.body.Camtinhvien,
+        TNV: req.body.TNV,
+        Hoivien: req.body.Hoivien,
+        Huongdanvien: req.body.Huongdanvien,
+        Huanluyenvien: req.body.Huanluyenvien,
+        Canbotangcuong: req.body.Canbotangcuong,
+        Tailieu: null,
+        TailieuHistory: null,
+        LinkTest: null,
+      });
+      res
+        .status(200)
+        .send({ success: true, message: "Tạo thành công thông tin!" });
+    } else {
+      Association.update(
+        {
           Chutichhoi: req.body.Chutichhoi,
           Phochutich_1: req.body.Phochutich_1,
           Phochutich_2: req.body.Phochutich_2,
@@ -42,277 +64,206 @@ exports.UpdateAssociation = (req, res) => {
           Huongdanvien: req.body.Huongdanvien,
           Huanluyenvien: req.body.Huanluyenvien,
           Canbotangcuong: req.body.Canbotangcuong,
-          Tailieu: null,
-          TailieuHistory: null,
-          LinkTest: null,
-        })
-          .then(() => {
-            res
-              .status(200)
-              .send({ success: true, message: "Tạo thành công thông tin!" });
-          })
-          .catch((err) => {
-            res.status(500).send({ success: false, message: err });
-          });
-      } else {
-        Association.update(
-          {
-            Chutichhoi: req.body.Chutichhoi,
-            Phochutich_1: req.body.Phochutich_1,
-            Phochutich_2: req.body.Phochutich_2,
-            Phochutich_3: req.body.Phochutich_3,
-            Phochutich_4: req.body.Phochutich_4,
-            Uyvien_1: req.body.Uyvien_1,
-            Uyvien_2: req.body.Uyvien_2,
-            Namthanhlap: req.body.Namthanhlap,
-            Ngaytruyenthong: req.body.Ngaytruyenthong,
-            Camtinhvien: req.body.Camtinhvien,
-            TNV: req.body.TNV,
-            Hoivien: req.body.Hoivien,
-            Huongdanvien: req.body.Huongdanvien,
-            Huanluyenvien: req.body.Huanluyenvien,
-            Canbotangcuong: req.body.Canbotangcuong,
+        },
+        {
+          where: {
+            id: 1,
           },
-          {
-            where: {
-              id: 1,
-            },
-          }
-        )
-          .then(() => {
-            res.status(200).send({
-              success: true,
-              message: "Cập nhập thành công thông tin!",
-            });
-          })
-          .catch((err) => {
-            res.status(500).send({ success: false, message: err });
-          });
-      }
-    })
-    .catch((err) => {
-      res.status(500).send({ success: false, message: err });
-    });
+        }
+      );
+      res.status(200).send({
+        success: true,
+        message: "Cập nhập thành công thông tin!",
+      });
+    }
+  } catch (error) {
+    res.status(500).send({ success: false, message: "Error -> " + error });
+  }
 };
 
-exports.viewPDF = (req, res) => {
-  Association.findOne({
-    where: {
-      id: 1,
-    },
-    attributes: ["Tailieu", "TailieuHistory"],
-  })
-    .then((association) => {
-      res.status(200).send({ success: true, message: association });
-    })
-    .catch((err) => {
-      res.status(500).send({ success: false, message: err });
+exports.viewPDF = async (req, res) => {
+  try {
+    let association = await Association.findOne({
+      where: {
+        id: 1,
+      },
+      attributes: ["Tailieu", "TailieuHistory"],
     });
+    res.status(200).send({ success: true, message: association });
+  } catch (error) {
+    res.status(500).send({ success: false, message: "Error -> " + error });
+  }
 };
 
-exports.editPDF = (req, res) => {
-  Association.findOne({
-    where: {
-      id: 1,
-    },
-  })
-    .then((association) => {
-      if (!association) {
-        Association.create({
-          id: 1,
-          Chutichhoi: null,
-          Phochutich_1: null,
-          Phochutich_2: null,
-          Phochutich_3: null,
-          Phochutich_4: null,
-          Uyvien_1: null,
-          Uyvien_2: null,
-          Namthanhlap: null,
-          Ngaytruyenthong: null,
-          Camtinhvien: null,
-          TNV: null,
-          Hoivien: null,
-          Huongdanvien: null,
-          Huanluyenvien: null,
-          Canbotangcuong: null,
+exports.editPDF = async (req, res) => {
+  try {
+    let association = await Association.findOne({
+      where: {
+        id: 1,
+      },
+    });
+    if (!association) {
+      Association.create({
+        id: 1,
+        Chutichhoi: null,
+        Phochutich_1: null,
+        Phochutich_2: null,
+        Phochutich_3: null,
+        Phochutich_4: null,
+        Uyvien_1: null,
+        Uyvien_2: null,
+        Namthanhlap: null,
+        Ngaytruyenthong: null,
+        Camtinhvien: null,
+        TNV: null,
+        Hoivien: null,
+        Huongdanvien: null,
+        Huanluyenvien: null,
+        Canbotangcuong: null,
+        Tailieu: req.body.Tailieu,
+        TailieuHistory: null,
+        LinkTest: null,
+      });
+      res
+        .status(200)
+        .send({ success: true, message: "Tạo thành công thông tin!" });
+    } else {
+      Association.update(
+        {
           Tailieu: req.body.Tailieu,
-          TailieuHistory: null,
-          LinkTest: null,
-        })
-          .then(() => {
-            res
-              .status(200)
-              .send({ success: true, message: "Tạo thành công thông tin!" });
-          })
-          .catch((err) => {
-            res.status(500).send({ success: false, message: err });
-          });
-      } else {
-        Association.update(
-          {
-            Tailieu: req.body.Tailieu,
+        },
+        {
+          where: {
+            id: 1,
           },
-          {
-            where: {
-              id: 1,
-            },
-          }
-        )
-          .then(() => {
-            res.status(200).send({
-              success: true,
-              message: "Cập nhập thành công thông tin!",
-            });
-          })
-          .catch((err) => {
-            res.status(500).send({ success: false, message: err });
-          });
-      }
-    })
-    .catch((err) => {
-      res.status(500).send({ success: false, message: err });
-    });
+        }
+      );
+      res.status(200).send({
+        success: true,
+        message: "Cập nhật thành công thông tin!",
+      });
+    }
+  } catch (error) {
+    res.status(500).send({ success: false, message: "Error -> " + error });
+  }
 };
 
-exports.editPDFHistory = (req, res) => {
-  Association.findOne({
-    where: {
-      id: 1,
-    },
-  })
-    .then((association) => {
-      if (!association) {
-        Association.create({
-          id: 1,
-          Chutichhoi: null,
-          Phochutich_1: null,
-          Phochutich_2: null,
-          Phochutich_3: null,
-          Phochutich_4: null,
-          Uyvien_1: null,
-          Uyvien_2: null,
-          Namthanhlap: null,
-          Ngaytruyenthong: null,
-          Camtinhvien: null,
-          TNV: null,
-          Hoivien: null,
-          Huongdanvien: null,
-          Huanluyenvien: null,
-          Canbotangcuong: null,
-          Tailieu: null,
+exports.editPDFHistory = async (req, res) => {
+  try {
+    let association = await Association.findOne({
+      where: {
+        id: 1,
+      },
+    });
+    if (!association) {
+      Association.create({
+        id: 1,
+        Chutichhoi: null,
+        Phochutich_1: null,
+        Phochutich_2: null,
+        Phochutich_3: null,
+        Phochutich_4: null,
+        Uyvien_1: null,
+        Uyvien_2: null,
+        Namthanhlap: null,
+        Ngaytruyenthong: null,
+        Camtinhvien: null,
+        TNV: null,
+        Hoivien: null,
+        Huongdanvien: null,
+        Huanluyenvien: null,
+        Canbotangcuong: null,
+        Tailieu: null,
+        TailieuHistory: req.body.TailieuHistory,
+        LinkTest: null,
+      });
+      res
+        .status(200)
+        .send({ success: true, message: "Tạo thành công thông tin!" });
+    } else {
+      Association.update(
+        {
           TailieuHistory: req.body.TailieuHistory,
-          LinkTest: null,
-        })
-          .then(() => {
-            res
-              .status(200)
-              .send({ success: true, message: "Tạo thành công thông tin!" });
-          })
-          .catch((err) => {
-            res.status(500).send({ success: false, message: err });
-          });
-      } else {
-        Association.update(
-          {
-            TailieuHistory: req.body.TailieuHistory,
+        },
+        {
+          where: {
+            id: 1,
           },
-          {
-            where: {
-              id: 1,
-            },
-          }
-        )
-          .then(() => {
-            res.status(200).send({
-              success: true,
-              message: "Cập nhập thành công thông tin!",
-            });
-          })
-          .catch((err) => {
-            res.status(500).send({ success: false, message: err });
-          });
-      }
-    })
-    .catch((err) => {
-      res.status(500).send({ success: false, message: err });
-    });
+        }
+      );
+      res.status(200).send({
+        success: true,
+        message: "Cập nhập thành công thông tin!",
+      });
+    }
+  } catch (error) {
+    res.status(500).send({ success: false, message: "Error -> " + error });
+  }
 };
 
-exports.editLinkTest = (req, res) => {
-  Association.findOne({
-    where: {
-      id: 1,
-    },
-  })
-    .then((association) => {
-      if (!association) {
-        Association.create({
-          id: 1,
-          Chutichhoi: null,
-          Phochutich_1: null,
-          Phochutich_2: null,
-          Phochutich_3: null,
-          Phochutich_4: null,
-          Uyvien_1: null,
-          Uyvien_2: null,
-          Namthanhlap: null,
-          Ngaytruyenthong: null,
-          Camtinhvien: null,
-          TNV: null,
-          Hoivien: null,
-          Huongdanvien: null,
-          Huanluyenvien: null,
-          Canbotangcuong: null,
-          Tailieu: null,
-          TailieuHistory: null,
+exports.editLinkTest = async (req, res) => {
+  try {
+    let association = await Association.findOne({
+      where: {
+        id: 1,
+      },
+    });
+    if (!association) {
+      Association.create({
+        id: 1,
+        Chutichhoi: null,
+        Phochutich_1: null,
+        Phochutich_2: null,
+        Phochutich_3: null,
+        Phochutich_4: null,
+        Uyvien_1: null,
+        Uyvien_2: null,
+        Namthanhlap: null,
+        Ngaytruyenthong: null,
+        Camtinhvien: null,
+        TNV: null,
+        Hoivien: null,
+        Huongdanvien: null,
+        Huanluyenvien: null,
+        Canbotangcuong: null,
+        Tailieu: null,
+        TailieuHistory: null,
+        LinkTest: req.body.LinkTest,
+      });
+      res
+        .status(200)
+        .send({ success: true, message: "Tạo thành công thông tin!" });
+    } else {
+      Association.update(
+        {
           LinkTest: req.body.LinkTest,
-        })
-          .then(() => {
-            res
-              .status(200)
-              .send({ success: true, message: "Tạo thành công thông tin!" });
-          })
-          .catch((err) => {
-            res.status(500).send({ success: false, message: err });
-          });
-      } else {
-        Association.update(
-          {
-            LinkTest: req.body.LinkTest,
+        },
+        {
+          where: {
+            id: 1,
           },
-          {
-            where: {
-              id: 1,
-            },
-          }
-        )
-          .then(() => {
-            res.status(200).send({
-              success: true,
-              message: "Cập nhập thành công thông tin!",
-            });
-          })
-          .catch((err) => {
-            res.status(500).send({ success: false, message: err });
-          });
-      }
-    })
-    .catch((err) => {
-      res.status(500).send({ success: false, message: err });
-    });
+        }
+      );
+      res.status(200).send({
+        success: true,
+        message: "Cập nhập thành công thông tin!",
+      });
+    }
+  } catch (error) {
+    res.status(500).send({ success: false, message: "Error -> " + error });
+  }
 };
 
-exports.viewLinkTest = (req, res) => {
-  Association.findOne({
-    where: {
-      id: 1,
-    },
-    attributes: ["LinkTest"],
-  })
-    .then((association) => {
-      res.status(200).send({ success: true, message: association });
-    })
-    .catch((err) => {
-      res.status(500).send({ success: false, message: err });
+exports.viewLinkTest = async (req, res) => {
+  try {
+    let association = await Association.findOne({
+      where: {
+        id: 1,
+      },
+      attributes: ["LinkTest"],
     });
+    res.status(200).send({ success: true, message: association });
+  } catch (error) {
+    res.status(500).send({ success: false, message: "Error -> " + error });
+  }
 };
--- server/controller/background.controller.js
+++ server/controller/background.controller.js
@@ -1,163 +1,186 @@
-const path = require('path')
-const fs = require('fs')
-var moment = require('moment')
-const db = require('../config/db.config')
-const replaceString = require('replace-string');
-var schedule = require('node-schedule');
-const Background = db.background
-const LogImage = db.logimage
-const Op = db.Sequelize.Op
-exports.AddBackground = (req, res) => {
-    const processedFile = req.file || {}
-    let orgName = processedFile.originalname || ''
-    orgName = orgName.trim().replace(/ /g, "-")
-    const fullPathInServ = processedFile.path
-    const newFullPath = `${fullPathInServ}-${orgName}`
+const path = require("path");
+const fs = require("fs");
+var moment = require("moment");
+const db = require("../config/db.config");
+const replaceString = require("replace-string");
+var schedule = require("node-schedule");
+const Background = db.background;
+const LogImage = db.logimage;
+const Op = db.Sequelize.Op;
+
+exports.AddBackground = async (req, res) => {
+  try {
+    const processedFile = req.file || {};
+    let orgName = processedFile.originalname || "";
+    orgName = orgName.trim().replace(/ /g, "-");
+    const fullPathInServ = processedFile.path;
+    const newFullPath = `${fullPathInServ}-${orgName}`;
     fs.renameSync(fullPathInServ, newFullPath);
 
-    var fileString = path.basename(newFullPath)
-    var filePath = `${process.env.SERVER_HOST}/api/background/` + fileString
+    let fileString = path.basename(newFullPath);
+    let filePath = `${process.env.SERVER_HOST}/api/background/` + fileString;
     const background = {};
-    if (req.body.tenchuongtrinh) background.Tenchuongtrinh = req.body.tenchuongtrinh; 
-    if (req.body.linkchuongtrinh) background.Linkchuongtrinh = req.body.linkchuongtrinh;
+    if (req.body.tenchuongtrinh)
+      background.Tenchuongtrinh = req.body.tenchuongtrinh;
+    if (req.body.linkchuongtrinh)
+      background.Linkchuongtrinh = req.body.linkchuongtrinh;
     background.Linkanh = filePath;
     if (req.body.ngaydienra) background.Ngaydienra = req.body.ngaydienra;
     if (req.body.ngayketthuc) background.Ngayketthuc = req.body.ngayketthuc;
     if (req.body.diadiem) background.Diadiem = req.body.diadiem;
-    // if (req.body.maunen) background.Maunen = req.body.maunen;
-    // if (req.body.mauchu) background.Mauchu = req.body.mauchu;
-    Background.findOne({
-        where:{
-            [Op.and] :[
-                {Tenchuongtrinh :req.body.tenchuongtrinh},
-                {Linkchuongtrinh :req.body.linkchuongtrinh},
-                {Linkanh :filePath},
-                {Ngaydienra :req.body.ngaydienra},
-                {Ngayketthuc: req.body.ngayketthuc},
-                {Diadiem: req.body.diadiem},
-                // {Maunen :req.body.maunen},
-                // {Mauchu :req.body.mauchu}
-            ]
-        }
+    let backgrounds = await Background.findOne({
+      where: {
+        [Op.and]: [
+          {
+            Tenchuongtrinh: req.body.tenchuongtrinh,
+          },
+          {
+            Linkchuongtrinh: req.body.linkchuongtrinh,
+          },
+          {
+            Linkanh: filePath,
+          },
+          {
+            Ngaydienra: req.body.ngaydienra,
+          },
+          {
+            Ngayketthuc: req.body.ngayketthuc,
+          },
+          {
+            Diadiem: req.body.diadiem,
+          },
+        ],
+      },
+    });
+    if (!backgrounds) {
+      new Background(backgrounds).save();
+      res.status(200).send({ success: true });
+    } else {
+      res.status(403).send({ success: false, message: "background is exist" });
+    }
+  } catch (error) {
+    res.status(500).send({ success: false, message: "Error -> " + error });
+  }
+};
+exports.ViewBackground = async (req, res) => {
+  try {
+    let backgrounds = await Background.findAll({
+      order: [["id", "DESC"]],
+    });
+    res.status(200).send({ success: true, data: backgrounds });
+  } catch (error) {
+    res.status(500).send({ success: false, message: "Error -> " + error });
+  }
+};
+exports.ViewOneBackground = async (req, res) => {
+  try {
+    let background = await Background.findOne({
+      where: {
+        id: req.query.backgroundId,
+      },
+    });
+    res.status(200).send({ success: true, data: background });
+  } catch (error) {
+    res.status(500).send({ success: false, message: "Error -> " + error });
+  }
+};
 
-    }).then(backgrounds =>{
-        if(!backgrounds) {
-            new Background(background).save()
-            .then(data => res.status(200).send({success : true}))
-            .catch(err => res.status(403).send({message: err}));
-        } else {
-            res.status(403).send({success: false, message: "background is exist"})
+exports.SlideShowBackground = async (req, res) => {
+  try {
+    let backgrounds = Background.findAll({
+      where: {
+        Ngayketthuc: {
+          [Op.gte]: moment().format("YYYY-MM-DD"),
+        },
+      },
+    });
+    res.status(200).send({ success: true, data: backgrounds });
+  } catch (error) {
+    res.status(500).send({ success: false, message: "Error -> " + error });
+  }
+};
+exports.EditBackground = async (req, res) => {
+  try {
+    let backgrounds = await Background.findOne({
+      where: {
+        id: parseInt(req.body.id),
+      },
+    });
+    if (!backgrounds)
+      res
+        .status(403)
+        .send({ success: false, message: "Background is not exists!" });
+    else {
+      Background.update(
+        {
+          Tenchuongtrinh: req.body.tenchuongtrinh,
+          Linkchuongtrinh: req.body.linkchuongtrinh,
+          Ngaydienra: req.body.ngaydienra,
+          Ngayketthuc: req.body.ngayketthuc,
+          Diadiem: req.body.diadiem,
+        },
+        {
+          where: {
+            id: req.body.id,
+          },
         }
-    }).catch(err => res.status(500).send({message: err}))
-}
-exports.ViewBackground = (req, res) => {
-    Background.findAll({
-        order: [
-            ['id', 'DESC']
-        ]
-    }).then( data => {
-        res.status(200).send({success: true, data: data})
-    }).catch(err => {
-        res.status(500).send({message: err})
-    })
-}
-exports.ViewOneBackground = (req, res) => {
-    Background.findOne({
-        where: {
-            id: req.query.backgroundId
-        }
-    }).then(information => {
-        res.status(200).send({success: true, data: information})
-    }).catch(err => {
-        res.status(500).send({success: false, message: err})
-    })
-}
-
-exports.SlideShowBackground = (req, res) => {
-    Background.findAll({
-        where: {
-            Ngayketthuc: {
-              [Op.gte]: moment().format('YYYY-MM-DD')
-            }
-        }
-    }).then( data => {
-        res.status(200).send({success: true, data: data})
-    }).catch(err => {
-        res.status(500).send({message: err})
-    })
-}
-exports.EditBackground = (req, res) =>{
-    Background.findOne({
-        where: {
-            id : parseInt(req.body.id)
-        }
-    }).then(background =>{
-        if(!background)
-            res.status(500).send({success: false, message: err})
-        else {
-            Background.update({
-                Tenchuongtrinh :req.body.tenchuongtrinh,
-                Linkchuongtrinh :req.body.linkchuongtrinh,
-                Ngaydienra :req.body.ngaydienra,
-                Ngayketthuc: req.body.ngayketthuc,
-                Diadiem: req.body.diadiem,
-                // Maunen :req.body.maunen,
-                // Mauchu :req.body.mauchu
-            }, {
-                where: {
-                    id : req.body.id
-                }
-            }).then(
-                res.status(200).send({success : true})
-            ).catch(err => {
-                res.status(500).send({success: false, message: err})
-            })
-        }
-    }).catch(err => res.status(500).send({success: false, message: err}))
-}
-exports.DeleteBackground = (req,res) =>{
-    Background.findOne({
-        where :{
-            id : req.body.id
-        }
-    }).then( background => {
-        const linkanh = {}
-        linkanh.Linkanh = background.Linkanh
-        new LogImage(linkanh).save()
-        Background.destroy({
+      );
+      res
+        .status(200)
+        .send({ success: true, data: "Update background is success!" });
+    }
+  } catch (error) {
+    res.status(500).send({ success: false, message: "Error -> " + error });
+  }
+};
+exports.DeleteBackground = async (req, res) => {
+  try {
+    let backgrounds = await Background.findOne({
+      where: {
+        id: req.body.id,
+      },
+    });
+    const linkanh = {};
+    linkanh.Linkanh = background.Linkanh;
+    new LogImage(linkanh).save();
+    let destroyBackground = Background.destroy({
+      where: {
+        id: req.body.id,
+      },
+    });
+    if (destroyBackground !== 0) {
+      res.status(200).send({ success: true });
+    } else {
+      res.status(403).status({ success: false, message: "An error occurred!" });
+    }
+  } catch (error) {
+    res.status(500).send({ success: false, message: "Error -> " + error });
+  }
+};
+var x = "!!!!!!";
+var j = schedule.scheduleJob(
+  {
+    hour: 3,
+    minute: 30,
+    dayOfWeek: 0,
+  },
+  function DeleteImageSystem() {
+    LogImage.findAll({}).then((data) => {
+      const host = `${process.env.SERVER_HOST}/api/background`;
+      data.map((y) => {
+        let a = y.Linkanh;
+        if (a == null) return false;
+        let x = replaceString(a, host, "./images");
+        fs.unlink(x, (err) => {
+          if (err) {
+          }
+          LogImage.destroy({
             where: {
-                id : req.body.id
-            }
-        }).then(function (result) {
-            if(result !== 0)
-            {
-                res.status(200).send({ success: true })
-            }
-            else
-            {
-                res.status(403).json({message: err})
-            }
-        })
-    }).catch(err => {})
-
-}
-var x = '!!!!!!';
-var j = schedule.scheduleJob({hour: 3, minute: 30, dayOfWeek: 0}, function DeleteImageSystem(){
-    LogImage.findAll({}).then(data =>{
-        const host = `${process.env.SERVER_HOST}/api/background`
-        data.map(y => {
-            let a = y.Linkanh
-            if(a == null) return false
-            let x = replaceString(a, host,'./images')
-            fs.unlink(x, (err) => {
-            if (err) {}
-            LogImage.destroy({
-                where: {
-                    Linkanh : a
-                }
-            }).then(result =>{})
-            })
-
-        })      
-    })
-}.bind(null,x));
+              Linkanh: a,
+            },
+          }).then((result) => {});
+        });
+      });
+    });
+  }.bind(null, x)
+);
